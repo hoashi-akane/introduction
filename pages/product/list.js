585015f3ef66392625cbabff9968c32df481e1ed
hoashi <akane.hoashi@outlook.jp>
2020-09-16 01:18:57 +0900

    変更：一覧表示用メソッド作成、呼び出し

--- pages/product/list.js
+++ pages/product/list.js
@@ -1,19 +1,33 @@
 import Layout from '../../components/layout'
 import { getProductList } from '../../lib/github/user'
 
+
 export default function ItemList({ allProduct }){
+    
+// ここでProductListの処理を書いても良い
     return(
         <Layout>
+            { ProductList(allProduct) }
         </Layout>
     );
 }
 
 export async function getServerSideProps(context) {
-    const allProduct = await getProductList()
-
+    let allProduct = await getProductList()
     return {
         props: {
             allProduct
         }
     }
+}
+
+export function ProductList(allProduct){
+    let list = []
+// for~in ではなく for~of
+    for(let product of allProduct){
+        list.push(<li>リポジトリ名: {product.node.name} </li>)
+        list.push(<li>説明: {product.node.description} </li>)
+        list.push(<a href={product.node.url}>{product.node.url}</a>)
+    }
+    return(list)
 }
