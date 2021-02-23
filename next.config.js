6664cc0c02a03ab93e5c99dd27a75f7b89e0079a
hoashi <akane.hoashi@outlook.jp>
2021-02-24 05:25:02 +0900

    追加：gh-pages用

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   assetPrefix: !debug ? '/introduction/' : '',
+  
+  env:{
+    GITHUB_TOKEN:process.env.GITHUB_TOKEN
+  }
 }
